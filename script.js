ec1e38fe14452682bd3f0ac6488899b136efb5c3
Vijayalakshmi Shettar <91591870+Vijayaa24@users.noreply.github.com>
2021-12-03 23:40:30 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+function goodmorning(){
+    var display=document.getElementById("display");
+    display.innerText="Good Morning";
+    var adddisplay=document.getElementById("adddisplay")
+    adddisplay.innerText="  Vijayalakshmi"
+    display.style.color="tomato"
+    adddisplay.style.color="green"
+}
+function goodafternoon(){
+    var display=document.getElementById("display");
+    display.innerText="Good Afternoon";
+    var adddisplay=document.getElementById("adddisplay")
+    adddisplay.innerText="  Vijayalakshmi"
+    display.style.color="tomato"
+    adddisplay.style.color="grey"
+}
+function goodnight(){
+    var display=document.getElementById("display");
+    display.innerText="Good Night";
+    var adddisplay=document.getElementById("adddisplay")
+    adddisplay.innerText="  Vijayalakshmi"
+    display.style.color="tomato"
+    adddisplay.style.color="violet"
+}
